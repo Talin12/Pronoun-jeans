a44c756aab5d6b7d374b839ad12f6d8fbb0ac589
Talin Daga <talindaga692@gmail.com>
2026-04-24 00:49:01 +0530

    feat: add Cloudinary storage for persistent media files

--- Backend/core/settings.py
+++ Backend/core/settings.py
@@ -26,6 +26,9 @@ INSTALLED_APPS = [
     'accounts',
     'products',
     'orders',
+
+    'cloudinary_storage',
+    'cloudinary',
 ]
 
 MIDDLEWARE = [
@@ -122,3 +125,14 @@ SIMPLE_JWT = {
     'USER_ID_FIELD': 'id',
     'USER_ID_CLAIM': 'user_id',
 }
+
+
+import cloudinary
+
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': env('CLOUDINARY_CLOUD_NAME'),
+    'API_KEY':    env('CLOUDINARY_API_KEY'),
+    'API_SECRET': env('CLOUDINARY_API_SECRET'),
+}
+
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
